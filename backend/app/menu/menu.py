03c0ae24cb3cf57ba96770fc0a4fda71dad46f2a
FabricioMoreno <fabricio.morenonaranjo@hotmail.com>
2023-09-04 21:43:37 -0500

    menu api fixed

--- backend/app/menu/menu.py
+++ backend/app/menu/menu.py
@@ -15,42 +15,52 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from datetime import date, datetime
 from sqlalchemy import or_, and_
 
-
 @bp.route('/getMenu', methods=['GET'])
 @cross_origin()
 @jwt_required()
 def getMenu():
     try:
-        #get user id
+        # Obtener el ID del usuario autenticado
         current_user = get_jwt_identity()
-        #get user
-        usuario = SAMM_Usuario.query.filter_by(Codigo=current_user).first()
-        #get rol
-        rol = usuario.IdPerfil
-        #get opciones
-        opciones = SAMM_OpRol.query.filter_by(idRol=rol).all()
-        #use squema to serialize
-        opcion_schema = []
-        for opcion in opciones:
-            gOpcion = SAMM_Opcion.query.filter(and_(SAMM_Opcion.Id == opcion.IdOpcion, SAMM_Opcion.Estado == 'A')).first()
-            if gOpcion:
-                # put path as '''if id is 1
-                # put path as gOpcion.Descripcion.replace(" ", "-").lower().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u") else
-                tryPath=gOpcion.Descripcion.replace(" ", "-").lower().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u")
-                path= tryPath
-                opcion_schema.append({
-                    'Id': gOpcion.Id,
-                    'Codigo': gOpcion.Codigo,
-                    'Estado': gOpcion.Estado,
-                    'FechaCrea': gOpcion.FechaCrea,
-                    'UsuarioCrea': gOpcion.UsuarioCrea,
-                    'FechaModifica': gOpcion.FechaModifica,
-                    'UsuarioModifica': gOpcion.UsuarioModifica,
-                    'FechaUltimoLogin': gOpcion.FechaUltimoLogin,
-                    'Descripcion': gOpcion.Descripcion,
-                    'Icon': gOpcion.Icon,
-                    'path': path
-                })
+
+        # Consulta SQL para obtener las opciones de menú
+        opciones = (
+            db.session.query(
+                SAMM_Opcion.Id,
+                SAMM_Opcion.Codigo,
+                SAMM_Opcion.Estado,
+                SAMM_Opcion.FechaCrea,
+                SAMM_Opcion.UsuarioCrea,
+                SAMM_Opcion.FechaModifica,
+                SAMM_Opcion.UsuarioModifica,
+                SAMM_Opcion.FechaUltimoLogin,
+                SAMM_Opcion.Descripcion,
+                SAMM_Opcion.Icon
+            )
+            .join(SAMM_OpRol, SAMM_Opcion.Id == SAMM_OpRol.IdOpcion)
+            .join(SAMM_Usuario, SAMM_Usuario.IdPerfil == SAMM_OpRol.idRol)
+            .filter(SAMM_Usuario.Codigo == current_user, SAMM_Opcion.Estado == 'A')
+            .all()
+        )
+
+        # Serializar las opciones en una lista de objetos JSON
+        opcion_schema = [
+
+            {
+                'Id': opcion.Id,
+                'Codigo': opcion.Codigo,
+                'Estado': opcion.Estado,
+                'FechaCrea': opcion.FechaCrea,
+                'UsuarioCrea': opcion.UsuarioCrea,
+                'FechaModifica': opcion.FechaModifica,
+                'UsuarioModifica': opcion.UsuarioModifica,
+                'FechaUltimoLogin': opcion.FechaUltimoLogin,
+                'Descripcion': opcion.Descripcion,
+                'Icon': opcion.Icon
+            }
+            for opcion in opciones
+        ]
+
         return jsonify(opcion_schema), 200
     except Exception as e:
         return jsonify({'message': str(e)}), 500
