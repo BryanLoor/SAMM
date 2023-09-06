9ef9aa627ff38484b237a7eed8640e1d94c5269b
FabricioMoreno <fabricio.morenonaranjo@hotmail.com>
2023-09-06 18:14:52 -0500

    Show all bitacora frontend fixed

--- frontend/pages/visitas/index.tsx
+++ frontend/pages/visitas/index.tsx
@@ -71,22 +71,6 @@ const VisitasPage = () => {
       width: 90,
       renderCell: (params) => <div>{detectVistaStatus(params.row.Estado)}</div>,
     },
-
-    {
-      field: "NombresVisitante",
-      headerName: "Status visita",
-      width: 150,
-    },
-    {
-      field: "ApellidosVisitante",
-      headerName: "Apellidos Visitante",
-      width: 150,
-    },
-    {
-      field: "Codigo",
-      headerName: "Identificacion Visitante",
-      width: 150,
-    },
     {
       field: "NombresAnfitrion",
       headerName: "Nombres Anfitrion",
@@ -98,49 +82,123 @@ const VisitasPage = () => {
       width: 150,
     },
     {
-      field: "Antecedentes",
-      headerName: "Antecedented Penales",
+      field: "NombresVisitante",
+      headerName: "Nombres Visitante",
+      width: 150,
+    },
+    {
+      field: "ApellidosVisitante",
+      headerName: "Apellidos Visitante",
+      width: 150,
+    },
+    {
+      field: "IdentificacionVisitante",
+      headerName: "Identificacion Visitante",
+      width: 150,
+    },
+    {
+      field: "Antecedentes Penales",
+      headerName: "Antecedentes Penales",
       width: 150,
       renderCell: (params) => (
-        <div>{params.row.Antecedentes ? "Si" : "No"}</div>
+        <div>{params.row.AntecdentesPenales ? "Si" : "No"}</div>
       ),
     },
     {
-      field: "Telefono",
+      field: "Ubicacion",
+      headerName: "Propiedad",
+      width: 150,
+    },
+    {
+      field: "FechaTimeVisitaEstimada",
+      headerName: "Fecha de llegada agendada",
+      width: 250,
+      renderCell: (params) => {
+        if (params.row.FechaTimeVisitaEstimada) {
+          const date = formatearFecha(params.row.FechaTimeVisitaEstimada);
+          return <div>{date}</div>;
+        } else {
+          return <div>No existe fecha</div>;
+        }
+      },
+    },
+    {
+      field: "FechaTimeVisitaReal",
+      headerName: "Fecha de llegada marcada",
+      width: 200,
+      renderCell: (params) => {
+        console.log(params.row.FechaTimeVisitaReal, "--------");
+        if (params.row.FechaTimeVisitaReal) {
+          const date = formatearFecha(params.row.FechaTimeVisitaReal);
+          return <div>{date}</div>;
+        } else {
+          return (
+            <div style={{ margin: "0 auto" }}>
+              <button
+                style={{
+                  color: "white",
+                  background: "#1e6479",
+                  padding: "6px",
+                  borderRadius: "5px",
+                }}
+              >
+                Marcar llegada
+              </button>
+            </div>
+          );
+        }
+      },
+    },
+    {
+      field: "FechaTimeSalidaEstimada",
+      headerName: "Fecha de salida agendada",
+      width: 250,
+      renderCell: (params) => {
+        if (params.row.FechaTimeSalidaEstimada) {
+          const date = formatearFecha(params.row.FechaTimeSalidaEstimada);
+          return <div>{date}</div>;
+        } else {
+          return <div>No existe fecha</div>;
+        }
+      },
+    },
+    {
+      field: "FechaTimeSalidaReal",
+      headerName: "Fecha de salida marcada",
+      width: 200,
+      renderCell: (params) => {
+        console.log(params.row.FechaTimeVisitaReal, "--------");
+        if (params.row.FechaTimeVisitaReal) {
+          const date = formatearFecha(params.row.FechaTimeSalidaReal);
+          return <div>{date}</div>;
+        } else {
+          return (
+            <div style={{ margin: "0 auto" }}>
+              <button
+                style={{
+                  color: "white",
+                  background: "#1e6479",
+                  padding: "6px",
+                  borderRadius: "5px",
+                }}
+              >
+                Marcar salida
+              </button>
+            </div>
+          );
+        }
+      },
+    },
+
+    {
+      field: "Celular",
       headerName: "Celular",
       width: 150,
     },
     {
-      field: "Ubicacion",
-      headerName: "Propiedad Ingreso",
+      field: "Correo",
+      headerName: "Correo",
       width: 150,
-    },
-    {
-      field: "FechaVisita",
-      headerName: "Hora Ingreso",
-      width: 150,
-      renderCell: (params) => {
-        const date = formatearFecha(params.row.FechaVisita);
-        return <div>{date}</div>;
-      },
-    },
-    {
-      field: "FechaSalidaEstimada", //confirmar
-      headerName: "Hora Salida",
-      width: 150,
-      renderCell: (params) => {
-        const date = formatearFecha(params.row.FechaSalidaEstimada);
-        return <div>{date}</div>;
-      },
-    },
-    {
-      field: "FechaSalidaEstimada",
-      headerName: "Hora Estimada",
-      width: 150,
-      renderCell: (params) => {
-        const date = formatearFecha(params.row.FechaSalidaEstimada);
-        return <div>{date}</div>;
-      },
     },
     {
       field: "Placa",
@@ -162,9 +220,9 @@ const VisitasPage = () => {
   const [allBitacoraVisitas, setAllBitacoraVisitas] = useState([]);
   const getAllBitacoras = async () => {
     try {
-      let allVisitas = await get("/visitas/obtenerAllBitacoraVisitas");
+      let allVisitas = await get("/visitas/getAllBitacoraVisitas");
       console.log(allVisitas);
-      setAllBitacoraVisitas(allVisitas);
+      setAllBitacoraVisitas(allVisitas.data);
     } catch (error) {
       console.error("No se pudo cargar las visitas");
     }
